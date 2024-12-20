69436a6620de1be4e5022512184191a925a59842
Janyk <me@janyk.dev>
2024-12-20 20:35:33 +0100

    chore: update splash image and clean up project files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 import { LoadingScreen } from '@/components/LoadingScreen';
 import { AuthProvider } from '@/context/auth';
 
